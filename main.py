fc93669d585469d65d58aa78e2da714636eccb96
Yevhen Dmytrenko <zdmitrenko26@gmail.com>
2026-04-06 23:57:16 +0300

    Solution

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import init_django_orm  # noqa: F401
 
-from db.models import Race, Skill, Player, Guild
+#from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
+    pass
     pass
     pass
 
