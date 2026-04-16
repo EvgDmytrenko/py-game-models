f80606115ee8e4327ff0fe3766950c4b30775bf4
Yevhen Dmytrenko <zdmitrenko26@gmail.com>
2026-04-17 00:53:11 +0300

    Solution

--- main.py
+++ main.py
@@ -13,32 +13,36 @@ def main() -> None:
         if race_dict:
             race_obj, race_created = Race.objects.get_or_create(
                 name=race_dict.get("name"),
-                description=race_dict.get("description"),
+                defaults={"description": race_dict.get("description")},
             )
 
-        skills_list = info.get("skills")
-        if skills_list:
-            for skill in skills_list:
-                Skill.objects.get_or_create(
-                    name=skill.get("name"),
-                    bonus=skill.get("bonus"),
-                    race=race_obj,
-                )
+            skills_list = race_dict.get("skills")
+            if skills_list:
+                for skill in skills_list:
+                    Skill.objects.get_or_create(
+                        name=skill.get("name"),
+                        defaults={
+                            "bonus": skill.get("bonus"),
+                            "race": race_obj,
+                        }
+                    )
 
-        guilds_dict = info.get("guilds")
+        guilds_dict = info.get("guild")
         if guilds_dict:
             guild_obj, guild_created = Guild.objects.get_or_create(
                 name=guilds_dict.get("name"),
-                description=guilds_dict.get("description"),
+                defaults={"description": guilds_dict.get("description")},
             )
 
         Player.objects.get_or_create(
             nickname=player,
-            email=info.get("email"),
-            bio=info.get("bio"),
-            race=race_obj if race_dict else None,
-            guild=guild_obj if guilds_dict else None
-            )
+            defaults={
+                "email": info.get("email"),
+                "bio": info.get("bio"),
+                "race": race_obj if race_dict else None,
+                "guild": guild_obj if guilds_dict else None
+            }
+        )
 
 
 if __name__ == "__main__":
