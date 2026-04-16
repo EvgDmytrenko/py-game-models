0fbaa483d4d566f82029cc93c4a0efbdc1577793
Yevhen Dmytrenko <zdmitrenko26@gmail.com>
2026-04-16 23:57:27 +0300

    Solution

--- main.py
+++ main.py
@@ -4,15 +4,41 @@ import json
 from db.models import Race, Skill, Player, Guild
 
 
-
 def main() -> None:
     with open("players.json", "r") as file:
         data = json.load(file)
 
+    for player, info in data.items():
+        race_dict = info.get("race")
+        if race_dict:
+            race_obj, race_created = Race.objects.get_or_create(
+                name=race_dict.get("name"),
+                description=race_dict.get("description"),
+            )
 
+        skills_list = info.get("skills")
+        if skills_list:
+            for skill in skills_list:
+                Skill.objects.get_or_create(
+                    name=skill.get("name"),
+                    bonus=skill.get("bonus"),
+                    race=race_obj,
+                )
 
+        guilds_dict = info.get("guilds")
+        if guilds_dict:
+            guild_obj, guild_created = Guild.objects.get_or_create(
+                name=guilds_dict.get("name"),
+                description=guilds_dict.get("description"),
+            )
 
-
+        Player.objects.get_or_create(
+            nickname=player,
+            email=info.get("email"),
+            bio=info.get("bio"),
+            race=race_obj if race_dict else None,
+            guild=guild_obj if guilds_dict else None
+            )
 
 
 if __name__ == "__main__":
